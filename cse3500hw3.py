3e79bae5f0073852c165b46de01c75a39f2aec30
katesnow1 <ksnow1038@gmail.com>
2025-10-23 17:23:26 -0400

    modified:   cse3500hw3.py

--- cse3500hw3.py
+++ cse3500hw3.py
@@ -3,12 +3,38 @@ def maxTriangle(triArr):
     if len(triArr) == 1:
         return triArr[0][0]
     #start at second to last row
-    print(triArr[-2])
+    for row in range(len(triArr) - 2, -1, -1):
+        for num in range(len(triArr[row])):
+            if triArr[row+1][num] > triArr[row+1][num+1]:
+                triArr[row][num] = triArr[row][num] + triArr[row+1][num]
+            else:
+                triArr[row][num] = triArr[row][num] + triArr[row+1][num+1]
+        triArr.pop()
+    return triArr[0][0]
+
+triangle = [
+    [7],
+    [3, 8],
+    [8, 1, 0],
+    [2, 7, 4, 4],
+    [4, 5, 2, 6, 5]
+]
+
+print(maxTriangle(triangle))
 
 
+def coinCombos(coins):
+    combos = {0}
+    for num in coins:
+        newCombos = set()
+        for combo in combos:
+            newCombos.add(num+combo)
+        combos = combos | newCombos
+    combos.pop()
+    return combos
 
-triArr = [[7], [3,8], [8,1,0], 
-          [2,7,4,4], [4,5,2,6,5]]
-baseCase = [[4]]
 
-print(maxTriangle(baseCase))
+k = [2,4,7]
+
+print(coinCombos(k))
+
